a44802e734893ebb6d730e22736eacab11daf902
TheXXcoder <carolvianaxx@gmail.com>
2023-03-02 19:18:24 -0300

    Restruturação do projeto

---
+++ estruturaConta.java
@@ -0,0 +1,27 @@
+class Conta {
+
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;  }
+/*
+>Uma classe é uma especificação de um tipo, definindo atributos e comportamentos.
+>Um objeto é uma instância de uma classe onde podemos definir valores para seus atributos. 
+>Para criar uma instância precisamos usar a palavra chave new.  */
+
+public class estruturaConta {
+
+    public static void main(String[] args) {
+        
+        Conta primeiroCliente =  new Conta();
+        primeiroCliente.saldo = 200;
+        System.out.println(primeiroCliente.saldo);
+
+        primeiroCliente.saldo += 100;  // > primeiroCliente.saldo += 100; OBS: a posição do + em relação ao = altera o valor final. ( +=  =+ )
+        System.out.println(primeiroCliente.saldo);
+
+        
+
+    }
+    
+}
