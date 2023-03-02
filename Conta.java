2a0b31780a370353cee716d72f313f62442e889c
TheXXcoder <carolvianaxx@gmail.com>
2023-03-02 17:12:55 -0300

    Atualização de classe

---
+++ Conta.java
@@ -0,0 +1,13 @@
+class Conta {
+
+    public static void main(String[] args) {
+        
+        double saldo;
+        int agencia;
+        int numero;
+        String titular;
+
+        
+}
+    
+}
--- WolfBank.java
+++ WolfBank.java
@@ -3,6 +3,8 @@ public class WolfBank {
     public static void main(String[] args) {
 
         
+
+
         
     }
     
